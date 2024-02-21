b9f9af71be394a77ea41bd38d2cd72c17157f7fc
Enrico.Schuetz <enrico.schuetz@evosoft.com>
2024-02-21 11:25:22 +0100

    chore: list summary of dependencies used in all binaries

---
+++ build/list-dependencies.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+# Parameters for this script, which are usually explicitly set in the pipeline:
+#
+# FILENAME_PREFIX: "tm-catalog-cli" per default and normally set to the repo name in a pipeline
+
+
+filename_prefix=${FILENAME_PREFIX:-"tm-catalog-cli"}
+files=`find ./  -maxdepth 1 -name "${filename_prefix}*"`
+
+echo -e "Used dependencies of compiled binaries\n${files}"
+
+total=""
+nl=$'\n'
+
+for file in ${files}
+do
+  dep_info=$(go version -m ${file} | grep -o 'dep.*')
+
+  readarray -t arr_dep_info <<< $dep_info
+
+  for dep_line in "${arr_dep_info[@]}"
+  do
+    arr_dep_line=($dep_line)
+    total+="${arr_dep_line[1]}@${arr_dep_line[2]}${nl}"
+  done
+done
+
+echo "${total}" | sort -u
