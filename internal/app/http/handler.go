47ee7f2b56f5e264aeb9240f7b2e231f3dc74905
Alex Borodin <alex@borodin.cc>
2024-12-12 14:04:09 +0100

    fixup: update CHANGELOG.md and openapi spec, return tm+json fetching by fetch name

--- internal/app/http/handler.go
+++ internal/app/http/handler.go
@@ -93,7 +93,7 @@ func (h *TmcHandler) GetThingModelByFetchName(w http.ResponseWriter, r *http.Req
 		return
 	}
 
-	HandleByteResponse(w, r, http.StatusOK, MimeJSON, data)
+	HandleByteResponse(w, r, http.StatusOK, MimeTMJSON, data)
 }
 
 // GetInventoryByID returns the metadata of a single TM by ID
