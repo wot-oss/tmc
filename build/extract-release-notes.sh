491debc8f8bcccc75da8a2e93c60dbd48d9b5f2e
Enrico.Schuetz <enrico.schuetz@evosoft.com>
2024-12-17 16:48:02 +0100

    chore(cicd): support prereleases

--- build/extract-release-notes.sh
+++ build/extract-release-notes.sh
@@ -1,19 +1,20 @@
 #! /bin/bash
 
-# Paramter to this script, which is usually explicitly set in the pipeline:
+# Parameter to this script, which is usually explicitly set in the pipeline:
 #
 # VERSION: version to extract from the CHANGELOG.md, usually the tag name of the release
+# NOTE_REQUIRED (optional): fails the script if the version to extract cannot be found in the CHANGELOG.md
 
 if [ -z "$VERSION" ]; then
   echo "build/extract-release-notes.sh failed: \$VERSION env var empty."
   exit 1
 fi
- 
+
 SED_PATTERN='/VERSION/,/^## /{/^## /!p}'
 SED_PATTERN=${SED_PATTERN/VERSION/${VERSION}}
-NOTE=$(sed -n "$SED_PATTERN" CHANGELOG.md) 
+NOTE=$(sed -n "$SED_PATTERN" CHANGELOG.md)
 
-if [ -z "$NOTE" ]; then
+if [ -z "$NOTE" ] && [ $NOTE_REQUIRED ]; then
   echo "build/extract-release-notes.sh failed: extracted empty string from CHANGELOG.md"
   exit 1
 fi
--- docker/download-cli.sh
+++ docker/download-cli.sh
@@ -1,6 +1,19 @@
 #! /bin/sh
 
-URL=$(curl -s https://api.github.com/repos/wot-oss/tmc/releases/latest | jq -r '.assets | .[] | select(.name == "tmc-linux-amd64") | .browser_download_url')
+
+# Parameter to this script, which is usually explicitly set in the pipeline:
+#
+# VERSION: version of the tmc CLI release to download
+
+if [ -z "$VERSION" ]; then
+  echo "download CLI failed: \$VERSION env var empty."
+  exit 1
+fi
+
+
+echo "download CLI for version: $VERSION"
+
+URL=$(curl -s https://api.github.com/repos/wot-oss/tmc/releases/tags/$VERSION | jq -r '.assets | .[] | select(.name == "tmc-linux-amd64") | .browser_download_url')
 
 curl -OL $URL
 mv ./tmc-linux-amd64 ./tmc
